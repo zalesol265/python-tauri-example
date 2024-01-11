f1b52615859dd1fa483c251e235ff967faa08cb7
Olivia Zaleski <zalesol265@gmail.com>
2024-01-10 19:02:48 -0500

    Input args from form submission in html

--- py/test.py
+++ py/test.py
@@ -1,9 +1,23 @@
-import sys    
+import sys  
+import json  
 
 arguments = sys.argv[1:]
 
 name = arguments[0]
-fruit1 = arguments[1]
-fruit2 = arguments[2]
+arg1 = arguments[1]
+arg2 = arguments[2]
 
-print(arguments[1])
+# Create sentences
+sentence1 = "Hello, {}! Greetings from Python!".format(name)
+sentence2 = "The first arg is {} and the second is {}".format(arg1, arg2)
+# Create a dictionary to represent the JSON object
+json_data = {
+    "greeting": sentence1,
+    "arguments": sentence2
+}
+
+# Convert the dictionary to JSON format
+json_output = json.dumps(json_data)
+
+# Print the JSON output
+print(json_output)
--- src/main.js
+++ src/main.js
@@ -1,20 +1,44 @@
 const { Command } = window.__TAURI__.shell;
 
 let greetMsgEl;
+let argumentsMsgEl;
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetMsgEl = document.querySelector("#greet-msg");
+  greetMsgEl = document.querySelector("#greetMsg");
+  argumentsMsgEl = document.querySelector("#argMsg");
 });
 
-document.querySelector("#greet-btn").addEventListener("click", greet);
+// document.querySelector("#greet-btn").addEventListener("click", greet);
 
 async function greet() {
-  const args = ["Olivia", "pineapples", "bananas"];
+  // Get values from input fields
+  const nameVal = document.getElementById("name").value;
+  const arg1 = document.getElementById("arg1").value;
+  const arg2 = document.getElementById("arg2").value;
+
+  // Check if any input is empty
+  if (!nameVal || !arg1 || !arg2) {
+    alert("Please fill in all the fields");
+    return;
+  }
+
+  // Pass user-input values to Python script
+  const args = [nameVal, arg1, arg2];
   const command = Command.sidecar("bin/python/test", args );
   const output = await command.execute();
   const { stdout, stderr } = output;
 
-  greetMsgEl.textContent = "Hello " + stdout;
+  // Parse the JSON output
+  const jsonOutput = JSON.parse(stdout);
+
+  // Access individual pieces of information
+  const greetingMsg = jsonOutput.greeting;
+  const argumentsMsg = jsonOutput.arguments;
+
+  // Update the DOM elements
+  greetMsgEl.textContent = greetingMsg;
+  argumentsMsgEl.textContent = argumentsMsg;
+
 }
 
 window.greet = greet;
